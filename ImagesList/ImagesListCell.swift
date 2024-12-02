0985fcc546065e19ccad151f38906e3e36c89742
RinaFV <vasilyeva.irina18@gmail.com>
2024-12-02 19:59:41 +0300

    Промежуточный результат

---
+++ ImagesList/ImagesListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Irina Vasileva on 02.12.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+
+}
